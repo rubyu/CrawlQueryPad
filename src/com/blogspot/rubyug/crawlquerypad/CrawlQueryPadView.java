417ffa0134e399e61300873cc5b0f046523616f6
rubyu <rubyu@sorr>
2010-05-27 03:16:18 +0900

    OPリスト出力まで。

--- src/com/blogspot/rubyug/crawlquerypad/CrawlQueryPadView.java
+++ src/com/blogspot/rubyug/crawlquerypad/CrawlQueryPadView.java
@@ -135,6 +135,9 @@ public class CrawlQueryPadView extends FrameView {
     }
 
     void queryParse() {
+
+        System.out.println( "--------------------" );
+
         //disconnect undomanager
         queryPaneDoc.removeUndoableEditListener(undomanager);
         
@@ -169,12 +172,145 @@ public class CrawlQueryPadView extends FrameView {
         try {
           SimpleNode query = parser.parse();
 
-          //dump
-          StringBuffer sb = new StringBuffer();
+          //
+          List<Object[]> stack;
+          int depth;
+
+          //pass 1
+          //ノードのナンバリングのためのリスト
+          List<SimpleNode> nodes = new ArrayList<SimpleNode>();
+          //命令リスト
+          List<String> operations = new ArrayList<String>();
+          //エラーリスト
           List<Throwable> throwables = new ArrayList<Throwable>();
-          List<Object[]> stack = new ArrayList<Object[]>();
+
+          //initialize
+          stack = new ArrayList<Object[]>();
           stack.add( new Object[]{query, -1} );
-          int depth = 0;
+          depth = 0;
+          for(;;) {
+            if ( depth == -1 ) {
+              break;
+            }
+            Object[] o = stack.get(depth);
+            SimpleNode node = (SimpleNode)o[0];
+            int cursor = (Integer)o[1];
+            cursor++;
+            o[1] = cursor;
+            if (cursor == 0) { //at first visit
+              //add node
+              nodes.add(node);
+              if (null != node.jjtGetValue()) { //if node has value
+                Object[] arr = (Object[])node.jjtGetValue();
+                Throwable throwable = (Throwable)arr[0];
+                if (null != throwable) {
+                  //add throwable
+                  throwables.add(throwable);
+                }
+              }
+            }
+            if ( node.jjtGetNumChildren() <= cursor ) {
+
+              //add operation
+              if ( node instanceof Condition ) {
+                operations.add( "[CONDITION] " + nodes.indexOf(node.jjtGetParent()) + ", " + nodes.indexOf(node) );
+              } else if ( node instanceof Condition_AND ) {
+                operations.add( "[CONDITION AND] " + nodes.indexOf(node.jjtGetParent()) + ", " + nodes.indexOf(node) );
+              } else if ( node instanceof Condition_Match ) {
+                operations.add( "[CONDITION MATCH] " + nodes.indexOf(node.jjtGetParent()) + ", " + nodes.indexOf(node) );
+              } else if ( node instanceof Condition_OR ) {
+                operations.add( "[CONDITION OR] " + nodes.indexOf(node.jjtGetParent()) + ", " + nodes.indexOf(node) );
+              } else if ( node instanceof Condition_Unary_NOT ) {
+                operations.add( "[CONDITION UNARY NOT] " + nodes.indexOf(node.jjtGetParent()) + ", " + nodes.indexOf(node) );
+
+              } else if ( node instanceof Crawl ) {
+                operations.add( "[CRAWL] " + nodes.indexOf(node.jjtGetParent()) + ", " + nodes.indexOf(node) );
+              } else if ( node instanceof Crawl_Execute ) {
+                operations.add( "[CRAWL EXECUTE] " + nodes.indexOf(node.jjtGetParent()) + ", " + nodes.indexOf(node) );
+              } else if ( node instanceof Crawl_Filter ) {
+                operations.add( "[CRAWL FILTER] " + nodes.indexOf(node.jjtGetParent()) + ", " + nodes.indexOf(node) );
+              } else if ( node instanceof Crawl_Operator_Difference ) {
+                operations.add( "[CRAWL OP DIFFERENCE] " + nodes.indexOf(node.jjtGetParent()) + ", " + nodes.indexOf(node) );
+              } else if ( node instanceof Crawl_Operator_Intersection ) {
+                operations.add( "[CRAWL OP INTERSECTION] " + nodes.indexOf(node.jjtGetParent()) + ", " + nodes.indexOf(node) );
+              } else if ( node instanceof Crawl_Operator_SymmetricDefference ) {
+                operations.add( "[CRAWL OP SYMMETRIC DEFFERENCE] " + nodes.indexOf(node.jjtGetParent()) + ", " + nodes.indexOf(node) );
+              } else if ( node instanceof Crawl_Operator_Union ) {
+                operations.add( "[CRAWL OP UNION] " + nodes.indexOf(node.jjtGetParent()) + ", " + nodes.indexOf(node) );
+              } else if ( node instanceof Crawl_Set_Condition ) {
+                operations.add( "[CRAWL SET CONDITION] " + nodes.indexOf(node.jjtGetParent()) + ", " + nodes.indexOf(node) );
+
+              } else if ( node instanceof Depth_Value ) {
+                Object[] arr = (Object[])node.jjtGetValue();
+                String value = (String)arr[2];
+                operations.add( "[DEPTH VALUE] " + nodes.indexOf(node.jjtGetParent()) + ", " + value );
+
+              } else if ( node instanceof Field_BODY ) {
+                operations.add( "[FIELD BODY] " + nodes.indexOf(node.jjtGetParent()) + ", FIELD_BODY" );
+              } else if ( node instanceof Field_ID ) {
+                operations.add( "[FIELD ID] " + nodes.indexOf(node.jjtGetParent()) + ", FIELD_ID" );
+              } else if ( node instanceof Field_LINKWORD ) {
+                operations.add( "[FIELD LINKWORD] " + nodes.indexOf(node.jjtGetParent()) + ", FIELD_LINKWORD" );
+              } else if ( node instanceof Field_TEXT ) {
+                operations.add( "[FIELD TEXT] " + nodes.indexOf(node.jjtGetParent()) + ", FIELD_TEXT" );
+              } else if ( node instanceof Field_TITLE ) {
+                operations.add( "[FIELD TITLE] " + nodes.indexOf(node.jjtGetParent()) + ", FIELD_TITLE" );
+              } else if ( node instanceof Field_URL ) {
+                operations.add( "[FIELD URL] " + nodes.indexOf(node.jjtGetParent()) + ", FIELD_URL" );
+
+              } else if ( node instanceof Index_Order ) {
+                operations.add( "[INDEX ORDER] " + nodes.indexOf(node.jjtGetParent()) + ", " + nodes.indexOf(node) );
+
+              } else if ( node instanceof Order_ASC ) {
+                operations.add( "[ORDER ASC] " + nodes.indexOf(node.jjtGetParent()) + ", ORDER_ASC" );
+              } else if ( node instanceof Order_DESC ) {
+                operations.add( "[ORDER DESC] " + nodes.indexOf(node.jjtGetParent()) + ", ORDER_DESC" );
+
+              } else if ( node instanceof Query ) {
+                operations.add( "[QUERY] " + "- , " + nodes.indexOf(node) );
+
+              } else if ( node instanceof Query_Option_Index ) {
+                operations.add( "[QUERY OPTION INDEX] " + nodes.indexOf(node.jjtGetParent()) + ", " + nodes.indexOf(node) );
+
+              } else if ( node instanceof Regex ) {
+                operations.add( "[REGEX] " + nodes.indexOf(node.jjtGetParent()) + ", " + nodes.indexOf(node) );
+              } else if ( node instanceof Regex_Option_Value ) {
+                Object[] arr = (Object[])node.jjtGetValue();
+                String value = (String)arr[2];
+                operations.add( "[REGEX OPTION VALUE] " + nodes.indexOf(node.jjtGetParent()) + ", " + value );
+              }  else if ( node instanceof Regex_Pattern_Value ) {
+                Object[] arr = (Object[])node.jjtGetValue();
+                String value = (String)arr[2];
+                operations.add( "[REGEX PATTERN VALUE] " + nodes.indexOf(node.jjtGetParent()) + ", " + value );
+
+              } else if ( node instanceof Url_Value ) {
+                Object[] arr = (Object[])node.jjtGetValue();
+                String value = (String)arr[2];
+                operations.add( "[URL] " + nodes.indexOf(node.jjtGetParent()) + ", " + value );
+
+              } else if ( node instanceof Urls ) {
+                operations.add( "[URLS] " + nodes.indexOf(node.jjtGetParent()) + ", " + nodes.indexOf(node) );
+              }
+
+              stack.remove(depth);
+              depth--;
+            } else {
+              //depth-first
+              stack.add( new Object[]{ node.jjtGetChild(cursor), -1 } );
+              depth++;
+            }
+          }
+
+          for (String s: operations) {
+            System.out.println( s );
+          }
+
+          //pass 2
+          StringBuffer sb = new StringBuffer();
+          //initialize
+          stack = new ArrayList<Object[]>();
+          stack.add( new Object[]{query, -1} );
+          depth = 0;
           for(;;) {
             if ( depth == -1 ) {
               break;
@@ -186,28 +322,20 @@ public class CrawlQueryPadView extends FrameView {
             o[1] = cursor;
 
             if (cursor == 0) { //at first visit
+              
               //write node name
               for ( int i=0; i < depth; i++ ) {
                 sb.append(" ");
               }
               sb.append(node.toString());
-              
+
               if (null != node.jjtGetValue()) { //if node has value
                 Object[] arr = (Object[])node.jjtGetValue();
                 Throwable throwable = (Throwable)arr[0];
                 Token token = (Token)arr[1];
                 String value = (String)arr[2];
-                if (null != throwable) {
-                  throwables.add(throwable);
-                }
+                
                 if (null != token) {
-                  /*
-                  sb.append(" [");
-                  sb.append("line " + token.beginLine + " column " + token.beginColumn);
-                  sb.append(" to ");
-                  sb.append("line " + token.endLine + " column " + token.endColumn);
-                  sb.append("]");
-                  */
                   //calculate node position
                   int startPos = -1;
                   for ( int i=0; i < token.beginLine - 1; i++ ) {
@@ -252,7 +380,7 @@ public class CrawlQueryPadView extends FrameView {
                     if (caretOnToken) {
                       StyleConstants.setBackground(attr, new Color(252,245,167) );
                     }
-                    
+
                   } else if (token.kind == QueryParserConstants.GT) {
                     StyleConstants.setBold(attr, true);
                     StyleConstants.setForeground(attr, new Color(55,59,62) ); //dark gray
@@ -262,7 +390,7 @@ public class CrawlQueryPadView extends FrameView {
                             ) {
                     StyleConstants.setBold(attr, true);
                     StyleConstants.setForeground(attr, new Color(85,86,88) ); //gray
-                    
+
                   } else if ( token.kind == QueryParserConstants.ORDER_ASC ||
                               token.kind == QueryParserConstants.ORDER_DESC
                             ) {
@@ -303,12 +431,15 @@ public class CrawlQueryPadView extends FrameView {
               depth++;
             }
           }
+
           logPane.setText( sb.toString() );
         } catch (Throwable t) {
           logPane.setText( ThrowableToString(t) );
         }
         //reconnect undomanager
         queryPaneDoc.addUndoableEditListener(undomanager);
+
+        System.out.println( "--------------------" );
     }
     void invokeQueryParse() {
         SwingUtilities.invokeLater(new Runnable() {
@@ -317,7 +448,7 @@ public class CrawlQueryPadView extends FrameView {
             }
         });
     }
-    class QueryPaneDocumentListener  implements DocumentListener {
+    class QueryPaneDocumentListener implements DocumentListener {
       public void insertUpdate(DocumentEvent e) {
         invokeQueryParse();
       }
