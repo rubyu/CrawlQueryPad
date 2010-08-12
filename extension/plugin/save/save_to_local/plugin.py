5a034b637229ee4ab9423af337090ab18bfc36e5
tukigase <tukigase@4th>
2010-08-12 18:17:41 +0900

    save_to_localで、ファイル名のダメ文字をエスケープするようにした。

--- extension/plugin/save/save_to_local/plugin.py
+++ extension/plugin/save/save_to_local/plugin.py
@@ -26,7 +26,65 @@ def _show_directory_dialog():
     if selected == JFileChooser.APPROVE_OPTION:
       file = filechooser.getSelectedFile()
       return file.getAbsolutePath()
-      
+
+def escape_filename(str):
+    #delete if reserved name
+    if str == "AUX" or \
+       str == "CLOCK$" or \
+       str == "COM1" or str == "COM2" or \
+       str == "COM3" or str == "COM4" or \
+       str == "COM5" or str == "COM6" or \
+       str == "COM7" or str == "COM8" or \
+       str == "COM9" or \
+       str == "CON" or \
+       str == "CONFIG$" or \
+       str == "LPT1" or str == "LPT2" or \
+       str == "LPT3" or str == "LPT4" or \
+       str == "LPT5" or str == "LPT6" or \
+       str == "LPT7" or str == "LPT8" or \
+       str == "LPT9" or \
+       str == "NUL" or \
+       str == "PRN":
+        str == ""
+    #delete if .. or .
+    if str == "." or str == "..":
+        str = ""
+    #remove last period or blank 
+    buf = []
+    stop = False
+    for s in reversed(str):
+        if not stop:
+            if s == "." or \
+               s == " " or \
+               s == "\t":
+                continue
+            else:
+                stop = True
+        buf.insert(0, s)
+    str = "".join(buf)
+    #replace character of ascii 0-31
+    buf = []
+    for s in str:
+        if 31 < ord(s):
+            buf.append(s)
+        else:
+            buf.append("_")
+    str = "".join(buf)
+    #replace reserved character
+    str = str.replace("\\", u"￥")
+    str = str.replace("/", u"／")
+    str = str.replace(":", u"：")
+    str = str.replace("*", u"＊")
+    str = str.replace("?", u"？")
+    str = str.replace("\"", u"”")
+    str = str.replace("<", u"＜")
+    str = str.replace(">", u"＞")
+    str = str.replace("|", u"｜")
+    #if empty
+    if str == "":
+        str = "(empty)"
+    return str
+    
 def call(API, data):
     """
     Main function of the extension.
@@ -58,11 +116,10 @@ def call(API, data):
     data.get("textFile")    #text file
     data.get("queryString") #query string
     """
-    title = data.get("title")
+    title     = data.get("title")
     textFile  = data.get("textFile")
     
-    state = API.getState()
-    
+    state = API.getState()    
     path = state.getFirstOr( "path", "" )
     if not File(path).exists():
         path = ""
@@ -73,11 +130,12 @@ def call(API, data):
             state.set( "path", path )
             API.setState(state)
     if "" != path:
-        f = open( File(path, title + ".txt").getPath(), "w" )
+        saveFile = File(path, escape_filename(title + ".txt"))
+        f = open( saveFile.getPath(), "w" )
         for line in open(textFile.getPath(), "r"):
             f.write( line )
         f.close()
-        return "Saved"
+        return "\"%s\" saved" % saveFile.getPath()
     else:
         return "Save Failed"
         
